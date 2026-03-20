bc4ac2cf4587a097c6a44ea6979cc7108e17f7e3
matheus-dev-fs <matheusmks10@gmail.com>
2026-03-20 18:20:26 -0300

    feat: complete item display with styling
    - Finished styling and formatting for item display in table
    - Added colored category badges based on category color
    - Displayed category titles and values with proper formatting
    - Applied conditional styling to values for expense/income
    - Ensured all item fields are shown in Brazilian date and currency format

--- src/components/body/table-area/table-item/table-item.component.tsx
+++ src/components/body/table-area/table-item/table-item.component.tsx
@@ -2,6 +2,7 @@ import { JSX } from 'react';
 import * as S from './table-item.styles';
 import { IItem } from '../../../../interfaces/item.interface';
 import { formatDate } from '../../../../helpers/date-filter.helper';
+import { categories } from '../../../../data/categories.data';
 
 type Props = {
     item: IItem;
@@ -11,9 +12,17 @@ export const TableItem = ({ item }: Props): JSX.Element => {
     return (
         <S.Row>
             <S.Column>{formatDate(item.date)}</S.Column>
-            <S.Column>{item.category}</S.Column>
+            <S.Column>
+                <S.Category $backgroudColor={categories[item.category].color}>
+                    {categories[item.category].title}
+                </S.Category>
+            </S.Column>
             <S.Column>{item.title}</S.Column>
-            <S.Column>R$ {item.value}</S.Column>
+            <S.Column>
+                <S.Value $expense={categories[item.category].expense}>
+                    RS {item.value}
+                </S.Value>
+            </S.Column>
         </S.Row>
     );
 }
--- src/components/body/table-area/table-item/table-item.styles.ts
+++ src/components/body/table-area/table-item/table-item.styles.ts
@@ -5,3 +5,15 @@ export const Row = styled.tr``;
 export const Column = styled.td`
     padding: 10px 0;
 `;
+
+export const Category = styled.div<{ $backgroudColor: string }>`
+    display: inline-block;
+    padding: 5px 10px;
+    border-radius: 5px;
+    color: #fff;
+    background-color: ${(props) => props.$backgroudColor};
+`;
+
+export const Value = styled.div<{ $expense: boolean }>`
+    color: ${(props) => (props.$expense ? '#ff0000' : '#00ff00')};
+`;
